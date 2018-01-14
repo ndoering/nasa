b37a2fcd5e6ca0625898f0eac7fae25faaf0266b
Nils D. Döring <mail@nils-doering.de>
2018-01-14 19:32:09 +0100

    Added simple command line parsing and error handling.
    Bug fix: fixed API URL

--- apod/apod.go
+++ apod/apod.go
@@ -7,21 +7,61 @@ import (
 	"net/http"
 )
 
+type ConnectionError struct {
+	msg  string
+	code int
+}
+
+func (c *ConnectionError) Error() string {
+	return c.msg
+}
+
+func newConnectionError(message string, code int) error {
+	return &ConnectionError{message, code}
+}
+
 type APODAnswer struct {
 	Date, Explanation, HDUrl, MediaType, ServiceVersion, Title, Url string
 }
 
 type Client struct {
 	key string
+	hd  bool
 }
 
-func NewClient(key string) Client {
-	return Client{key}
+func NewClient(key string, hd bool) Client {
+	return Client{key, hd}
 }
 
 func (c *Client) GetImage() (im image.Image, err error) {
-	response, err := http.Get("https://api.nasa.gov/planetary/nasa?hd&api_key=" + c.key)
+
+	url, err := getImageUrl(c.key, c.hd)
 	if err != nil {
+		return
+	}
+
+	response, err := http.Get(url)
+	if err != nil {
+		return
+	}
+
+	defer response.Body.Close()
+
+	im, err = jpeg.Decode(response.Body)
+
+	return
+}
+
+func getImageUrl(key string, hd bool) (url string, err error) {
+	response, err := http.Get("https://api.nasa.gov/planetary/apod?api_key=" + key)
+	if err != nil {
+		response.Body.Close()
+		return
+	}
+
+	if response.StatusCode != 200 {
+		err = newConnectionError("Access not allowed. Please check your API key.", response.StatusCode)
+		response.Body.Close()
 		return
 	}
 
@@ -32,14 +72,11 @@ func (c *Client) GetImage() (im image.Image, err error) {
 
 	response.Body.Close()
 
-	response, err = http.Get(m.HDUrl)
-	if err != nil {
-		return
+	if hd == true {
+		url = m.HDUrl
+	} else {
+		url = m.Url
 	}
-
-	defer response.Body.Close()
-
-	im, err = jpeg.Decode(response.Body)
 
 	return
 }
--- cmd/nasa-apod/main.go
+++ cmd/nasa-apod/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/ndoering/nasa/apod"
 	"image/jpeg"
@@ -8,11 +9,18 @@ import (
 )
 
 func main() {
-	c := apod.NewClient("DEMO_KEY")
+	var apiKey string
+	var hd bool
+	flag.StringVar(&apiKey, "key", "DEMO_KEY", "The API_KEY to use with NASA API")
+	flag.BoolVar(&hd, "hd", false, "Download the HD image")
+
+	flag.Parse()
+
+	c := apod.NewClient(apiKey, hd)
 
 	im, _ := c.GetImage()
 
-	file, err := os.Create("/home/shogun/Downloads/test.jpg")
+	file, err := os.Create("test.jpg")
 	if err != nil {
 		fmt.Println(err)
 	}
